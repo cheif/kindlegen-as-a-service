5a80694aa8b86c884e9959deb20c8b398356f124
Dan Berglund <dan@berglund.io>
2021-07-24 22:38:39 +0200

    A few smaller updates:
    1. Restructure code a bit, so it's easier to add tests.
    2. Handle errors a bit better (making sure we return 400 or error)
    3. Add failing test for epub with incorrect language.
    4. Handle above by reading exit code from kindlegen, and return stdout
    as error

---
+++ src/kindle.rs
@@ -0,0 +1,47 @@
+use std::process::Command;
+use std::fs;
+use std::str;
+use uuid::Uuid;
+
+pub fn convert(buf: Vec<u8>) -> Result<fs::File, String> {
+    let uuid = Uuid::new_v4();
+    let epub_file = format!("/tmp/{}.epub", uuid);
+    let mobi_file = format!("/tmp/{}.mobi", uuid);
+    println!("... Writing to disk ({})", &epub_file);
+    // Write the epub to disk, so that we can run kindlegen on it
+    fs::write(&epub_file, buf).map_err(|_| "Unable to write file")?;
+
+    println!("... Running kindlegen ({})", &epub_file);
+    let output = Command::new("kindlegen")
+        .arg(epub_file)
+        .output()
+        .map_err(|_| "Kindlegen run failed")?;
+
+    if !output.status.success() {
+        return Err(str::from_utf8(&output.stdout).unwrap().to_string())
+    }
+
+    println!("Returning to user");
+    return fs::File::open(mobi_file).map_err(|_| "Could not open resulting file".to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_epub() {
+        let buf = std::fs::read("testdata/hostregn.epub").unwrap();
+        let res = convert(buf);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn incorrect_language_code_epub() {
+        let buf = std::fs::read("testdata/pottraning_pa_3_daga.epub").unwrap();
+        let res = convert(buf);
+        println!("{:?}", res);
+        assert!(res.is_err());
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,37 @@
 #[macro_use]
 extern crate rouille;
+extern crate uuid;
 
-use std::process::Command;
 use std::fs;
 use std::io::Read;
+use rouille::Request;
 use rouille::Response;
+mod kindle;
 
 fn main() {
     println!("Starting server");
     rouille::start_server("0.0.0.0:8080", move |request| {
         router!(request,
                 (PUT) (/) => {
-                    println!("Starting request");
-                    let mut data = request.data().expect("Could not get body");
-
-                    let mut buf = Vec::new();
-                    match data.read_to_end(&mut buf) {
-                        Ok(_) => (),
-                        Err(_) => return Response::text("Failed to read body")
-                    };
-
-                    println!("... Writing to disk");
-                    // Write the epub to disk, so that we can run kindlegen on it
-                    fs::write("/tmp/file.epub", buf).expect("Unable to write file");
-
-                    println!("... Running kindlegen");
-                    Command::new("kindlegen")
-                        .arg("/tmp/file.epub")
-                        .output()
-                        .expect("Error when running kindlegen");
-
-
-                    println!("Returning to user");
-                    Response::from_file("application/mobi", fs::File::open("/tmp/file.mobi").unwrap())
+                    match process_request(request) {
+                        Ok(file) => Response::from_file("application/mobi", file),
+                        Err(error) => Response::text(error).with_status_code(400)
+                    }
                 },
                 _ => Response::empty_404()
                 )
     });
+}
+
+fn process_request(request: &Request) -> Result<fs::File, String> {
+    println!("Starting request");
+    let mut data = request.data().ok_or_else(|| "Could not get body")?;
+
+    let mut buf = Vec::new();
+    match data.read_to_end(&mut buf) {
+        Ok(_) => (),
+        Err(_) => return Err("Failed to read body".to_string())
+    };
+
+    return kindle::convert(buf);
 }
